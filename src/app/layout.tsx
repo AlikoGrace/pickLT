89bb9a1716b83a8216b5e779474ca8c46be01996
AhianyoElisha <elisha.ahianyo247@gmail.com>
2026-02-24 02:29:51 +0000

    light and dark mode reactivated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <AuthProvider>
               <MoveSearchProvider>{children}</MoveSearchProvider>
             </AuthProvider>
-            {/* <CustomizeControl /> */}
+            <CustomizeControl />
           </div>
         </ThemeProvider>
       </body>
